8bfe8b16881f46ba7a087bedf3827977f023c1fc
wahyuPM <wahyupratama191@gmail.com>
2024-01-04 22:04:10 +0800

    init project

---
+++ src/components/Player.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react"
+
+export default function Player({ initialName, symbol }) {
+    const [playerName, setPlayerName] = useState(initialName)
+    const [isEditing, setIsEditing] = useState(false)
+
+    const handleEditClick = () => {
+        //gunakan seperti ini jika update state berdasarkan state sebelumnya 
+        //jangan lakukan seperti ini setIsEditing(!isEditing)
+        setIsEditing((editing) => !editing)
+    }
+
+    const handleChange = (event) => {
+        setPlayerName(event.target.value)
+    }
+
+    return (
+        <li>
+            <span className="player">
+                {isEditing && <input type="text" required value={playerName} onChange={handleChange}></input>}
+                {!isEditing && <span className="player-name">{playerName}</span>}
+                <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
+        </li>
+    )
+}
