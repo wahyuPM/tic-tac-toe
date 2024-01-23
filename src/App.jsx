0b06b5e9d252abfa25ce6a41b9ee4192bf85002a
wahyuPM <wahyupratama191@gmail.com>
2024-01-23 23:37:53 +0800

    fix : typo component name

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 
 import Player from "./components/Player.jsx"
-import GameBoard from "./components/Gameboard.jsx"
+import GameBoard from "./components/GameBoard.jsx"
 import Log from "./components/Log.jsx"
 import GameOver from "./components/GameOver.jsx"
 
